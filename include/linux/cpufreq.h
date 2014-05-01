457506964e0ec47dd2a37f1125f3c43bf4f298c2
googyanas <googyanas@gmail.com>
2014-05-01 22:30:49 +0100

    cpufreq: add intellidemand governor (thx faux123)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -426,6 +426,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 
 #endif
 
