ef80d2c9683dc2cfcf090ecbc2a97864328eb770
GOOGY Anas <googyanas@gmail.com>
2014-05-21 00:02:10 +0100

    Stweaks: simpler customconfig xml

--- ramfs/res/uci.sh
+++ ramfs/res/uci.sh
@@ -5,54 +5,44 @@
 # You probably won't need to modify this file
 # You'll need to modify the files in /res/customconfig directory
 
-BB=/sbin/busybox
-
-ACTION_SCRIPTS=/res/customconfig/actions;
-source /res/customconfig/customconfig-helper;
+source /res/customconfig/customconfig-helper
 
 # first, read defaults
-read_defaults;
+read_defaults
 
 # read the config from the active profile
-read_config;
-
-UCI_PID=`pgrep "uci.sh"`;
-$BB renice -n -15 -p $UCI_PID;
-
-/sbin/busybox mount -o remount,rw /
-/sbin/busybox mount -o remount,rw /system
+read_config
 
 case "${1}" in
-	rename)
-    	rename_profile "${2}" "${3}";
-    	exit 0;
+  rename)
+    rename_profile "$2" "$3"
+    exit 0
     ;;
-	delete)
-    	delete_profile "${2}";
-    	exit 0;
+  delete)
+    delete_profile "$2"
+    exit 0
     ;;
-	select)
-    	select_profile "${2}";
-    	exit 0;
+  select)
+    select_profile "$2"
+    exit 0
     ;;
-	dump)
-	dump_profile "$2";
-	exit 0;
+  dump)
+    dump_profile "$2"
+    exit 0
     ;;
-	config)
-    	print_config;
+  config)
+    print_config
     ;;
-	list)
-    	list_profile;
+  list)
+    list_profile
     ;;
-	apply)
-		apply_config;
-	;;
-	*)
-		. ${ACTION_SCRIPTS}/${1} ${1} ${2} ${3} ${4} ${5} ${6};
-	;;
+  apply)
+    apply_config
+    ;;
+  *)
+    . ${ACTION_SCRIPTS}/$1 $1 $2 $3 $4 $5 $6
+    ;;
 esac;
 
 # write back the config to the active profile
-write_config;
-
+write_config
