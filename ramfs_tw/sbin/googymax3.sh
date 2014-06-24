e690922bae2140ce1b2ddd99b0ac2979d4657e36
googyanas <googyanas@gmail.com>
2014-06-24 20:45:22 +0100

    re-enable SWAP & SEC_OOM_KILLER

--- ramfs_tw/sbin/googymax3.sh
+++ ramfs_tw/sbin/googymax3.sh
@@ -169,9 +169,6 @@ fi;
 # enable kmem interface for everyone by GM
 echo "0" > /proc/sys/kernel/kptr_restrict;
 
-# swappiness
-echo "0" > /proc/sys/vm/swappiness
-
 # apply STweaks defaults
 export CONFIG_BOOTING=1
 /res/uci.sh apply
