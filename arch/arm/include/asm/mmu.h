37e02572a2bc3c0619fed3c49bbbb55b51a2993a
googyanas <googyanas@gmail.com>
2014-08-02 15:23:24 +0100

    Revert "ARM: Use TTBR1 instead of reserved context ID"
    This reverts commit b2baaa298df979465e0046f3b57eb1b41d6c0bef.

--- arch/arm/include/asm/mmu.h
+++ arch/arm/include/asm/mmu.h
@@ -5,15 +5,18 @@
 
 typedef struct {
 #ifdef CONFIG_CPU_HAS_ASID
-	atomic64_t	id;
+	unsigned int id;
+	raw_spinlock_t id_lock;
 #endif
 	unsigned int kvm_seq;
 } mm_context_t;
 
 #ifdef CONFIG_CPU_HAS_ASID
-#define ASID_BITS	8
-#define ASID_MASK	((~0ULL) << ASID_BITS)
-#define ASID(mm)	((mm)->context.id.counter & ~ASID_MASK)
+#define ASID(mm)	((mm)->context.id & 255)
+
+/* init_mm.context.id_lock should be initialized. */
+#define INIT_MM_CONTEXT(name)                                                 \
+	.context.id_lock    = __RAW_SPIN_LOCK_UNLOCKED(name.context.id_lock),
 #else
 #define ASID(mm)	(0)
 #endif
@@ -26,7 +29,7 @@ typedef struct {
  *  modified for 2.6 by Hyok S. Choi <hyok.choi@samsung.com>
  */
 typedef struct {
-	unsigned long	end_brk;
+	unsigned long		end_brk;
 } mm_context_t;
 
 #endif
@@ -36,8 +39,6 @@ typedef struct {
  * so enable interrupts over the context switch to avoid high
  * latency.
  */
-#ifndef CONFIG_CPU_HAS_ASID
 #define __ARCH_WANT_INTERRUPTS_ON_CTXSW
-#endif
 
 #endif
