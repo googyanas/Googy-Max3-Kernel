0d03da5ed4f00c70b6c2082abc271715e8b954ed
googyanas <googyanas@gmail.com>
2014-10-06 16:32:28 +0100

    cpufreq: Notify governors when cpus are hot-[un]plugged
    Because cpufreq core and governors worry only about the online cpus, if
    a cpu is
    hot [un]plugged, we must notify governors about it, otherwise be ready
    to expect
    something unexpected.
    
    We already have notifiers in the form of
    CPUFREQ_GOV_START/CPUFREQ_GOV_STOP, we
    just need to call them now.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -967,6 +967,8 @@ static int cpufreq_add_dev_policy(unsigned int cpu,
 				cpufreq_cpu_put(managed_policy);
 				return -EBUSY;
 			}
+			
+			__cpufreq_governor(managed_policy, CPUFREQ_GOV_STOP);
 
 			spin_lock_irqsave(&cpufreq_driver_lock, flags);
 			cpumask_copy(managed_policy->cpus, policy->cpus);
@@ -974,6 +976,10 @@ static int cpufreq_add_dev_policy(unsigned int cpu,
 					managed_policy->cpus, cpu_online_mask);
 			per_cpu(cpufreq_cpu_data, cpu) = managed_policy;
 			spin_unlock_irqrestore(&cpufreq_driver_lock, flags);
+			
+			__cpufreq_governor(managed_policy, CPUFREQ_GOV_START);
+			__cpufreq_governor(managed_policy, CPUFREQ_GOV_LIMITS);
+
 
 			pr_debug("CPU already managed, adding link\n");
 			ret = sysfs_create_link(&dev->kobj,
@@ -1259,8 +1265,11 @@ static int __cpufreq_remove_dev(struct device *dev, struct subsys_interface *sif
 	 */
 	if (unlikely(cpu != data->cpu)) {
 		pr_debug("removing link\n");
+		__cpufreq_governor(data, CPUFREQ_GOV_STOP);
 		cpumask_clear_cpu(cpu, data->cpus);
 		spin_unlock_irqrestore(&cpufreq_driver_lock, flags);
+		__cpufreq_governor(data, CPUFREQ_GOV_START);
+		__cpufreq_governor(data, CPUFREQ_GOV_LIMITS);
 		kobj = &dev->kobj;
 		cpufreq_cpu_put(data);
 		unlock_policy_rwsem_write(cpu);
